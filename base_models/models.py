4b0e19fba39ea03c0e08708ace58dbbe91771c87
Fernando Julca <fernandojulcap@gmail.com>
2025-08-08 20:34:06 -0500

    creando tablas en base_models

--- base_models/models.py
+++ base_models/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+from django.db import models
+
+class Appointment(models.Model):
+    # Aquí irán los campos 
+    pass
+
+class PaymentType(models.Model):
+    # Campos aquí
+    pass
+
+class Therapist(models.Model):
+    # Campos aquí
+    pass
+
+class UserVerificationCode(models.Model):
+    # Campos aquí
+    pass
+
