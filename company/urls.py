49f9b043b7b1e0b11763a26cdf2542ad9e176027
Fernando Julca <fernandojulcap@gmail.com>
2025-08-10 11:25:28 -0500

    configurando urls

--- company/urls.py
+++ company/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from .import views
 
 urlpatterns = [
-    
+    path('<int:company_id>/show/', views.show_company, name='show_company'),
+    path('store/', views.store_company, name='store_company'),
+    path('<int:company_id>/logo/upload/', views.upload_logo, name='upload_logo'),
+    path('<int:company_id>/logo/', views.show_logo, name='show_logo'),
+    path('<int:company_id>/logo/delete/', views.delete_logo, name='delete_logo'),
 ]
+
+
+
