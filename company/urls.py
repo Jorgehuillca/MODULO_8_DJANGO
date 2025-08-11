eca1bfadc63c3e47507f529f44441f46554d1ebe
Fernando Julca <fernandojulcap@gmail.com>
2025-08-10 23:52:58 -0500

    usando defaultrouter

--- company/urls.py
+++ company/urls.py
@@ -1,13 +1,14 @@
-from django.urls import path
-from .import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CompanyDataViewSet
+
+router = DefaultRouter()
+router.register(r'company', CompanyDataViewSet, basename='company')
 
 urlpatterns = [
-    path('<int:company_id>/show/', views.show_company, name='show_company'),
-    path('store/', views.store_company, name='store_company'),
-    path('<int:company_id>/logo/upload/', views.upload_logo, name='upload_logo'),
-    path('<int:company_id>/logo/', views.show_logo, name='show_logo'),
-    path('<int:company_id>/logo/delete/', views.delete_logo, name='delete_logo'),
+    path('', include(router.urls)),
 ]
+
 
 
 
