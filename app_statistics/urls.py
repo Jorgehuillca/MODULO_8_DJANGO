9b614bb4e771b8ef446142ff3f8f011d301ef2ea
Fernando Julca <fernandojulcap@gmail.com>
2025-08-11 18:21:19 -0500

    configurando urls

--- app_statistics/urls.py
+++ app_statistics/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from app_statistics.views import StatisticsViewSet
+from django.urls import path, include
+
+router = DefaultRouter()
+router.register(r'statistics', StatisticsViewSet, basename='statistics')
 
 urlpatterns = [
-    
+    path('',include(router.urls)),
 ]
